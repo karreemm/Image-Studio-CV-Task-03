34c13b29e37000bc7e7c4c38fcabcce984c6e2c2
kareem <kareem.abdelnabi.22@gmail.com>
2025-03-29 02:27:18 +0200

    Add Navigations in UI

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow 
+from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QStackedWidget
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QIcon
 from helper_functions.compile_qrc import compile_qrc
@@ -14,7 +14,27 @@ class MainWindow(QMainWindow):
         loadUi('main.ui', self)
         self.setWindowTitle('Image Studio')
         self.setWindowIcon(QIcon('icons_setup\icons\logo.png'))
+
+        # Add navigation Configuration
+        self.modesCombobox = self.findChild(QComboBox, 'modesCombobox')
+        self.modesCombobox.currentIndexChanged.connect(self.updateMode)
+
+        self.modesStackedWidget = self.findChild(QStackedWidget, 'modesStackedWidget')
+        self.framesStackedWidget = self.findChild(QStackedWidget, 'framesStackedWidget')
     
+
+    def updateMode(self):
+        current_mode = self.modesCombobox.currentText()
+        if current_mode == 'Extract The Unique Features':
+            self.framesStackedWidget.setCurrentIndex(0)
+            self.modesStackedWidget.setCurrentIndex(0)
+        elif current_mode == 'Generate Feature Descriptors':
+            self.framesStackedWidget.setCurrentIndex(1)
+            self.modesStackedWidget.setCurrentIndex(1)
+        elif current_mode == 'Match Two Images':
+            self.framesStackedWidget.setCurrentIndex(2)
+            self.modesStackedWidget.setCurrentIndex(2)
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
